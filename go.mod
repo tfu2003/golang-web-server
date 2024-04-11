b8137e8c9ccbd10219a3496e198e9c9504f10c1f
tfu2003 <officialtonyfu@gmail.com>
2024-04-10 18:10:55 -0700

    added server

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/tfu2003/golang-web-server
 
 go 1.21.2
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/go-chi/chi v1.5.5
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -3,19 +3,33 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 )
 
 func main() {
-	fmt.Println("Hello world")
-
-	godotenv.Load(".env")
+	godotenv.Load()
 
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	err := server.ListenAndServe()
+
+	if err != nil {
+		log.Fatal(err)
 	}
 
 	fmt.Println("Port:", portString)
