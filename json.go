a80f630f0dcad0225c1b30e2c76b371e2c499d7f
tfu2003 <officialtonyfu@gmail.com>
2024-04-26 00:05:11 -0700

    added error handler

--- json.go
+++ json.go
@@ -10,6 +10,13 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 	if code > 499 {
 		log.Println("Responding with 5XX error:", msg)
 	}
+	type errResponse struct {
+		Error string `json:"error"`
+	}
+
+	respondWithJSON(w, code, errResponse{
+		Error: msg,
+	})
 }
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
