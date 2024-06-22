b9c24cdfc888b684ff2e16d8d77d74292491bd99
tfu2003 <officialtonyfu@gmail.com>
2024-06-22 13:28:05 -0700

    added post request to get feed follow

--- main.go
+++ main.go
@@ -61,6 +61,7 @@ func main() {
 	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
 	v1Router.Post("/users", apiCfg.handlerCreateUser)
 	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
+	v1Router.Post("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerCreateFeedFollow))
 	router.Mount("/v1", v1Router)
 
 	server := &http.Server{
