1e6ea0b2e808d4f44dd729170d5b8fa25c4ccf6f
tfu2003 <officialtonyfu@gmail.com>
2024-06-22 01:58:32 -0700

    added abilty to make feeds

--- handler_feed.go
+++ handler_feed.go
@@ -10,9 +10,10 @@ import (
 	"github.com/tfu2003/golang-web-server/internal/database"
 )
 
-func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
+func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Request, user database.User) {
 	type parameters struct {
 		Name string `json:"name"`
+		URL string 	`json:"url"`
 	}
 	decoder := json.NewDecoder(r.Body)
 	
@@ -23,20 +24,18 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	user, err := apiCfg.DB.CreateUser(r.Context(), database.CreateUserParams{
+	feed, err := apiCfg.DB.CreateFeed(r.Context(), database.CreateFeedParams{
 		ID: uuid.New(),
 		CreatedAt: time.Now().UTC(),
 		UpdatedAt: time.Now().UTC(),
 		Name: params.Name,
+		Url: params.URL,
+		UserID: user.ID,
 	})
 	if (err != nil) {
-		respondWithError(w, 400, fmt.Sprintf("Could not create user: %v", err))
+		respondWithError(w, 400, fmt.Sprintf("Could not create feed: %v", err))
 		return
 	}
 
-	respondWithJSON(w, 201, databaseUserToUser(user))
-}
-
-func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request, user database.User) {
-	respondWithJSON(w, 200, databaseUserToUser(user))
+	respondWithJSON(w, 201, databaseFeedToFeed(feed))
 }
