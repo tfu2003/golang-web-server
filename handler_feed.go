846ab7f4c29c2332956113b18eba93601dbe8701
tfu2003 <officialtonyfu@gmail.com>
2024-06-22 11:07:51 -0700

    added query to get all feeds

--- handler_feed.go
+++ handler_feed.go
@@ -39,3 +39,13 @@ func (apiCfg *apiConfig) handlerCreateFeed(w http.ResponseWriter, r *http.Reques
 
 	respondWithJSON(w, 201, databaseFeedToFeed(feed))
 }
+
+func (apiCfg *apiConfig) handlerGetFeeds(w http.ResponseWriter, r *http.Request) {
+	feeds, err := apiCfg.DB.GetFeeds(r.Context())
+	if (err != nil) {
+		respondWithError(w, 400, fmt.Sprintf("Could not get feeds: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 201, databaseFeedsToFeeds(feeds))
+}
--- main.go
+++ main.go
@@ -58,6 +58,7 @@ func main() {
 	v1Router.Get("/healthz", handlerReadiness)
 	v1Router.Get("/error", handlerError)
 	v1Router.Get("/users", apiCfg.middlewareAuth(apiCfg.handlerGetUser))
+	v1Router.Get("/feeds", apiCfg.handlerGetFeeds)
 	v1Router.Post("/users", apiCfg.handlerCreateUser)
 	v1Router.Post("/feeds", apiCfg.middlewareAuth(apiCfg.handlerCreateFeed))
 	router.Mount("/v1", v1Router)
